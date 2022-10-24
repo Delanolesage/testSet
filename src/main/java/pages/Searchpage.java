89a072cdeb1fa8d008b1cd2d0a5642526ea35989
Olatundji Delano Ahononga <Olatundji-Delano.Ahononga@t-systems.com>
2022-10-24 15:06:51 +0200

    Update class Searchpage

--- src/main/java/pages/Searchpage.java
+++ src/main/java/pages/Searchpage.java
@@ -1,26 +1,31 @@
 package pages;
 
-import eu.tsystems.mms.tic.testframework.pageobjects.GuiElement;
 import eu.tsystems.mms.tic.testframework.pageobjects.Page;
-import eu.tsystems.mms.tic.testframework.webdrivermanager.WebDriverManager;
+import eu.tsystems.mms.tic.testframework.pageobjects.UiElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.List;
-
 public class Searchpage extends Page {
 
-    private final WebDriver driver = WebDriverManager.getWebDriver();;
+    //private final WebDriver driver = WebDriverManager.getWebDriver();;
     private WebDriverWait wait;
 
-    private final GuiElement searchTxt = new GuiElement(this.getWebDriver(), By.name("q"));
+    //private final GuiElement searchTxt = new GuiElement(this.getWebDriver(), By.name("q"));
 
-    private final GuiElement searchBtn = new GuiElement(this.getWebDriver(), By.id("search_button_homepage"));
+    //private final GuiElement searchBtn = new GuiElement(this.getWebDriver(), By.id("search_button_homepage"));
 
-    private final GuiElement videosLink = new GuiElement(this.getWebDriver(), By.linkText("Videos"));
+    //private final GuiElement videosLink = new GuiElement(this.getWebDriver(), By.linkText("Videos"));
 
-    private final GuiElement videos = new GuiElement(this.getWebDriver(), By.className("tile--vid"));
+    //private final GuiElement videos = new GuiElement(this.getWebDriver(), By.className("tile--vid"));
+
+    private UiElement searchTxt = find(By.name("q"));
+
+    private UiElement searchBtn = find(By.id("search_button_homepage"));
+
+    private UiElement videosLink = find(By.linkText("Videos"));
+
+    private UiElement allVideos = find(By.className("tile--vid"));
 
 
     public Searchpage(WebDriver driver) {
@@ -41,8 +46,8 @@ public class Searchpage extends Page {
     }
 
     public int getResult(){
-        List<GuiElement> allVideos = videos.getList();
-        System.out.println("Search Result : " + allVideos.size());
-        return allVideos.size();
+        //List<UiElement> allVideos = videos.getList();
+        System.out.println("Search Result : " + this.allVideos.list().size());
+        return allVideos.list().size();
     }
 }
