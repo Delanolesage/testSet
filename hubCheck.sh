c6dcf34f57112fe6b6cc8f956be05ade354eaf52
Olatundji Delano Ahononga <Olatundji-Delano.Ahononga@t-systems.com>
2022-11-02 13:50:13 +0100

    update files

--- hubCheck.sh
+++ hubCheck.sh
@@ -14,6 +14,6 @@ done
 # start the java command
 java -cp testSet.jar:testSet-tests.jar:libs/* \
     -DHUB_HOST=$HUB_HOST
-    -Dcucumber.features="$CUCUMBER_FEATURES" \
+    -Dcucumber.options="$CUCUMBER_FEATURES" \
     -Dtt.system.settings.file="$FILE" \
     org.testng.TestNG -testclass runner.TestRunner
