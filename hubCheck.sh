28ed295ad7a37cc6582800e21e0d2c29d399c071
Olatundji Delano Ahononga <Olatundji-Delano.Ahononga@t-systems.com>
2022-11-02 13:17:06 +0100

    update hubCheck

--- hubCheck.sh
+++ hubCheck.sh
@@ -6,13 +6,14 @@
 
 echo "Checking if hub is ready - $HUB_HOST"
 
-while [ "$( curl -s http://localhost:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
+while [ "$( curl -s http://$HUB_HOST:4444/wd/hub/status | jq -r .value.ready )" != "true" ]
 do
 	sleep 1
 done
 
 # start the java command
 java -cp testSet.jar:testSet-tests.jar:libs/* \
+    -DHUB_HOST=$HUB_HOST
     -Dcucumber.features="$CUCUMBER_FEATURES" \
     -Dtt.system.settings.file="$FILE" \
     org.testng.TestNG -testclass runner.TestRunner
