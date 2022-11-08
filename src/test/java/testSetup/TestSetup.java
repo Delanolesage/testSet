b0ecb55709636172cba138e69b1517b717ee12bf
Olatundji Delano Ahononga <Olatundji-Delano.Ahononga@t-systems.com>
2022-11-08 10:42:01 +0100

    Update Setup

--- src/test/java/testSetup/TestSetup.java
+++ src/test/java/testSetup/TestSetup.java
@@ -5,6 +5,7 @@ import eu.tsystems.mms.tic.testframework.testing.TesterraTest;
 import eu.tsystems.mms.tic.testframework.testing.WebDriverManagerProvider;
 import eu.tsystems.mms.tic.testframework.useragents.ChromeConfig;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 public class TestSetup extends TesterraTest implements WebDriverManagerProvider {
@@ -28,62 +29,9 @@ public class TestSetup extends TesterraTest implements WebDriverManagerProvider 
     }
 
 
-    /*
-    @BeforeSuite
-    private Properties checkProperties(String env) {
-
-        switch (env) {
-            case "staging":
-                System.out.println("Staging properties loaded.");
-                return loadProperties("test_" + env + ".properties");
-            case "null":
-                System.out.println("FMb properties loaded.");
-                return loadProperties("test_" + env + ".properties");
-            case "fmc":
-                System.out.println("FMc properties loaded.");
-                return loadProperties("test_" + env + ".properties");
-            default:
-                System.out.println("Default properties loaded.");
-        }
-
-        return null;
-    }*/
-
-    /*
-    @Override
-    public boolean loadProperties(String resourceFile) {
-        return false;
-    }*/
-
-
-    /*
-    @BeforeMethod
-    public void setupDriver() {
-        PROPERTY_MANAGER.loadProperties("test.properties");
-        DesktopWebDriverRequest request = new DesktopWebDriverRequest();
-        request.setBrowser(Browsers.chrome);
-        request.setShutdownAfterTest(false);
-        WebDriver driver = WEB_DRIVER_MANAGER.getWebDriver(request);
-        /*
-        // BROWSER => chrome / firefox
-        // HUB_HOST => localhost / 10.0.1.3 / hostname
-        String host = "localhost";
-        DesiredCapabilities dc;
-
-        if(System.getProperty("BROWSER") != null &&
-                System.getProperty("BROWSER").equalsIgnoreCase("firefox")){
-            dc = DesiredCapabilities.firefox();
-        }else{
-            dc = DesiredCapabilities.chrome();
-        }
-
-        if(System.getProperty("HUB_HOST") != null){
-            host = System.getProperty("HUB_HOST");
-        }
-
-        String completeUrl = "http://" + host + ":4444/wd/hub";
-        this.driver = new RemoteWebDriver(new URL(completeUrl), dc);
-
-    }*/
+    @AfterSuite(alwaysRun = true)
+    public void shutdownBrowser() {
+        WEB_DRIVER_MANAGER.shutdownAllThreadSessions();
+    }
 
 }
